d3c36ff74257aee1777a9f2041bc9596d6847716
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-05-21 17:05:01 +0600

    Kona fixed

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,7 +1,7 @@
 // Store the logged in user value
 let loggedInMentee = "";
 let loggedInMentor = "";
-
+let userType = "";
 // Function to set the logged in mentee
 export function setLoggedInMentee(mentee) {
   // Store the user value in localStorage
@@ -24,4 +24,16 @@ export function setLoggedInmentor(mentor) {
 export function getLoggedInmentor() {
   // Retrieve the user value from localStorage
   return localStorage.getItem("loggedInMentor");
+}
+
+// Function to set the logged in user
+export function setuserType(user) {
+  // Store the user value in localStorage
+  localStorage.setItem("userType", user);
+}
+
+// Function to get the logged in user
+export function getuserType() {
+  // Retrieve the user value from localStorage
+  return localStorage.getItem("userType");
 }
