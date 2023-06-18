962d802d0af8cac4070418970f5b8f8f20e70c1e
Shams Farabi Al Muhit <65844905+this-is-shams@users.noreply.github.com>
2023-06-18 17:58:21 +0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://skill-swap-backend.onrender.com', // Replace with your backend server URL
+      target: 'http://localhost:5000', // Replace with your backend server URL
       changeOrigin: true,
     })
   );
